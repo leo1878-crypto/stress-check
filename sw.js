94cf863a5a6cf732b0603c297386c0b335791fc2
leo1878-crypto <leo1878@gmail.com>
2026-03-10 13:04:49 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='ns-en-v1';
-const ASSETS=['/','index.html','manifest.json'];
+const CACHE='ns-en-v2';
+const ASSETS=['/','index.html','manifest.json','icon-192.png','icon-512.png'];
 
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
@@ -12,5 +12,11 @@ self.addEventListener('activate',e=>{
 });
 
 self.addEventListener('fetch',e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+  e.respondWith(
+    fetch(e.request).then(res=>{
+      const clone=res.clone();
+      caches.open(CACHE).then(c=>c.put(e.request,clone));
+      return res;
+    }).catch(()=>caches.match(e.request))
+  );
 });
